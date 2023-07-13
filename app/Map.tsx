2c5408291b26f6cf86fb8a106b078a2e4188ac0b
nnikolajj <greger.nikolaj@gmail.com>
2023-07-13 08:25:06 +0200

    Mapper

--- app/Map.tsx
+++ app/Map.tsx
@@ -1,15 +1,19 @@
-import {SafeAreaView} from "react-native";
+import {SafeAreaView, View, Text} from "react-native";
 
 import * as Location from 'expo-location';
-import MapView from "react-native-maps";
-import { Marker } from 'react-native-maps';
+import MapView, {Polyline} from "react-native-maps";
+import { Marker, Heatmap } from 'react-native-maps';
 import React, {useState} from "react";
 import {AntDesign} from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Map() {
 
-    const [trips, setTrips] = useState([{longitude: 9, latitude: 47.5, visited: false},{longitude: 8.7, latitude: 47.5, visited: true}])
+    const [trips, setTrips] = useState([{title: "Thurgau",longitude: 9, latitude: 47.5, visited: false},{title: "Raugau",longitude: 8.7, latitude: 47.5, visited: true}])
+
+    const [excursion, setExcursion] = useState([{title: "Yo Ho",longitude: 8.75, latitude: 47.4},{title: "Gilgamesh",longitude: 8.73, latitude: 47.5}])
+
+    const [zoom, setZoom] = useState(false)
 
     const [mapRegion, setmapRegion] = useState({
         latitude: 47.5,
@@ -18,13 +22,37 @@ export default function Map() {
         longitudeDelta: 0.0421,
     });
 
+    function zoomChange(e){
+
+        if (e.latitudeDelta * e.longitudeDelta <= 1.08){
+            setZoom(true);
+        }
+        else{
+            setZoom(false)
+        }
+
+    }
+
     return(
         <SafeAreaView>
             <MapView
                 style={{ alignSelf: 'stretch', height: '100%' }}
-                region={mapRegion}>
-                {/*trips.map((trip) => trip.visited ? <Marker coordinate={trip} title='Marker' > <FontAwesome name="rebel" size={38} color="#25292e"/></Marker> : <Marker coordinate={trip} title='Marker' > <FontAwesome name="question" size={38} color="#25292e"/></Marker>)*/}
+                region={mapRegion}
+            onRegionChange={(e) => zoomChange(e)}>
 
+                {zoom ?
+                        <View>
+                            {excursion.map((ex) =>
+                            <Marker coordinate={ex} title={ex.title} pinColor={"yellow"}/>
+                        )}
+                        </View> :
+                        <View>
+
+                            {trips.map((trip) => trip.visited ?
+                                <Marker coordinate={trip} title={trip.title} pinColor={"blue"}/>
+                                : <Marker coordinate={trip} title={trip.title}/>)}
+                        </View>
+                    }
             </MapView>
         </SafeAreaView>
     )
